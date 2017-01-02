c2d9dca6370a04dc8478761036eb815b13155842
Amio <amio.cn@gmail.com>
2017-01-02 22:02:57 +0800

    Initial commit.

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Uploader from '../components/uploader'
+
+export default () => (
+  <div className='content-wrapper'>
+    <div>Welcome to next.js!</div>
+    <Uploader />
+  </div>
+)
